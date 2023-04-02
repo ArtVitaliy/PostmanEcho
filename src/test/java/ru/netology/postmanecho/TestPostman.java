a02899aab27e3c61c44b21ffd11d2e8850bac41d
ArtVitaly <0331116@mail.ru>
2023-04-02 22:15:03 +0300

    success

--- src/test/java/ru/netology/postmanecho/TestPostman.java
+++ src/test/java/ru/netology/postmanecho/TestPostman.java
@@ -16,7 +16,7 @@ public class TestPostman {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some"))
+                .body("data", equalTo("some data"))
         ;
     }
 
