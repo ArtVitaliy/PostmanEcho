f8babb5fe1491558fa2017729fac7b2a8b31ece6
ArtVitaly <0331116@mail.ru>
2023-04-02 21:51:45 +0300

    Initial commit

---
+++ src/test/java/ru/netology/postmanecho/TestPostman.java
@@ -0,0 +1,24 @@
+package ru.netology.postmanecho;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestPostman {
+    @Test
+    void test1() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+
+}
